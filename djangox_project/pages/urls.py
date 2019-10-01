a9011cc78cd8cbf5b166089d7bcab0aafd103352
sweetlearn <info@sweetlearn.com>
2019-10-01 15:07:38 +0200

    import ProfilePageView to urls.py

--- djangox_project/pages/urls.py
+++ djangox_project/pages/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, ProfilePageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
