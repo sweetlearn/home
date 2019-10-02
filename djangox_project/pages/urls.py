66917ad94de60dd5bf4ca09ba0d66bb099499193
sweetlearn <info@sweetlearn.com>
2019-10-02 14:14:11 +0200

    urls change ProfilePageView

--- djangox_project/pages/urls.py
+++ djangox_project/pages/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPageView, ProfilePageView
-
+from .views import HomePageView, AboutPageView
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
